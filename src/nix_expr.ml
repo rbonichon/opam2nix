79ab4083b7330fff82b76530d3112736272833ab
Richard Bonichon <richard.bonichon@tweag.io>
2021-01-28 11:13:12 +0100

    Format-ted printer

--- src/nix_expr.ml
+++ src/nix_expr.ml
@@ -1,7 +1,7 @@
-module AttrSet  = struct
+module AttrSet = struct
   include Util.StringMap
 
-  let build pairs = List.fold_left (fun map (k, v) -> add k v map) empty pairs 
+  let build pairs = List.fold_left (fun map (k, v) -> add k v map) empty pairs
 
   let keys map = bindings map |> List.map fst
 end
@@ -65,40 +65,33 @@ let keysafe s = Str.string_match (Str.regexp "^[-a-zA-Z_][-a-zA-Z_0-9]*$") s 0
 
 let escape_key s = if keysafe s then s else "\"" ^ escape_string s ^ "\""
 
-let write dest (t : t) =
+let write ppf (t : t) =
   let open Format in
-  let formatter = formatter_of_out_channel dest in
   let indent_width = 2 in
-  let put = pp_print_string formatter in
-  let nl = pp_force_newline formatter in
-  let space = pp_print_space formatter in
-  let rec _write (t : t) =
+  let put = pp_print_string ppf in
+  let nl = pp_force_newline ppf in
+  let space = pp_print_space ppf in
+  let rec _write ppf (t : t) =
     let dbl = "\"" in
     let two_singles = "''" in
     let string_component (escape : string -> string) c =
       match c with
-      | `Lit s -> put (escape s)
-      | `Expr s ->
-          put "${";
-          _write s;
-          put "}"
+      | `Lit s -> pp_print_string ppf (escape s)
+      | `Expr s -> fprintf ppf "${%a}" _write s
     in
     let parens_if_needed part =
       match part with
       (* for neatness, we don't bother enclosing simple expressions in parens *)
       | `Id _ | `Int _ | `Lit _ | `String _ | `MultilineString _ | `List _
       | `Attrs _ | `Rec_attrs _ ->
-          _write part
-      | _ ->
-          put "(";
-          _write part;
-          put ")"
+          _write ppf part
+      | _ -> fprintf ppf "(%a)" _write part
     in
-    let property name = put ("." ^ escape_key name) in
+    let property name = fprintf ppf ".%s" (escape_key name) in
 
     let write_attrs ~prefix a =
-      pp_print_cut formatter ();
-      pp_open_box formatter indent_width;
+      pp_print_cut ppf ();
+      pp_open_box ppf indent_width;
       put prefix;
       put "{";
       a
@@ -107,9 +100,9 @@ let write dest (t : t) =
              nl ();
              put (if keysafe key then key else "\"" ^ escape_string key ^ "\"");
              put " = ";
-             _write v;
+             _write ppf v;
              put ";");
-      pp_close_box formatter ();
+      pp_close_box ppf ();
       nl ();
       put "}"
     in
@@ -125,30 +118,21 @@ let write dest (t : t) =
         put two_singles
     | `List parts ->
         put "[";
-        pp_open_box formatter indent_width;
+        pp_open_box ppf indent_width;
         space ();
         parts
         |> List.iteri (fun i part ->
                if i > 0 then space ();
                parens_if_needed part);
         space ();
-        pp_close_box formatter ();
+        pp_close_box ppf ();
         put "]"
     | `Id id -> put id
     | `Int i -> put (string_of_int i)
     | `Lit str -> put str
     | `Null -> put "null"
     | `BinaryOp (a, op, b) ->
-        let bracket expr =
-          put "(";
-          _write expr;
-          put ")"
-        in
-        bracket a;
-        put " ";
-        put op;
-        put " ";
-        bracket b
+        fprintf ppf "(%a) %a (%a)" _write a pp_print_string op _write b
     | `Property (src, name) ->
         parens_if_needed src;
         property name
@@ -156,65 +140,40 @@ let write dest (t : t) =
         parens_if_needed src;
         path |> List.iter property
     | `Property_or (src, name, alt) ->
-        _write (`Property (src, name));
-        put " or ";
-        _write alt
+        fprintf ppf "%a or %a" _write (`Property (src, name)) _write alt
     | `Function (args, body) ->
-        _write args;
-        put ":";
-        nl ();
-        _write body;
-        nl ()
+        fprintf ppf "@[<v>%a:@,%a@,@]" _write args _write body
     | `Call args ->
-        args
-        |> List.iteri (fun i arg ->
+         List.iteri (fun i arg ->
                if i > 0 then space ();
-               parens_if_needed arg)
+               parens_if_needed arg) args
     | `Let_bindings (vars, expr) ->
-        put "let";
-        (* pp_print_cut formatter (); *)
-        pp_open_box formatter 1;
-        vars
-        |> AttrSet.iter (fun key v ->
-               (* XXX what about quoted keys? *)
-               nl ();
-               put key;
-               put " = ";
-               _write v;
-               put ";");
-        pp_close_box formatter ();
-        nl ();
-        put "in";
-        nl ();
-        _write expr
+        fprintf ppf "@[<v>@[<v 2>let %a]@,in@,%a@]"
+          (fun ppf ->
+            AttrSet.iter (fun key v -> fprintf ppf "@,%s = %a;" key _write v))
+          vars _write expr
     | `NamedArguments parts ->
-        put "{ ";
-        pp_open_box formatter indent_width;
-        parts
-        |> List.iteri (fun i part ->
-               if i <> 0 then (
-                 put ",";
-                 space () );
-               match part with
-               | `Id arg -> put arg
-               | `Default (arg, exp) ->
-                   put arg;
-                   put " ? ";
-                   _write exp);
-        pp_close_box formatter ();
-        space ();
-        put "}"
+        fprintf ppf "{@ @[<%d>%a@]@ }" indent_width
+          (fun ppf ->
+            List.iteri (fun i part ->
+                if i <> 0 then fprintf ppf ",@ ";
+                match part with
+                | `Id arg -> pp_print_string ppf arg
+                | `Default (arg, e) -> fprintf ppf "@[%s ? %a@]" arg _write e))
+          parts
     | `Template parts ->
-        parts |> List.iter (function `Lit s -> put s | `Expr e -> _write e)
+       pp_print_list
+        (fun ppf -> function `Lit s -> pp_print_string ppf s | `Expr e -> _write ppf e)
+        ppf 
+          parts
     | `Rec_attrs a -> write_attrs ~prefix:"rec " a
     | `Attrs a -> write_attrs ~prefix:"" a
     | `With (scope, expr) ->
-        put "with ";
-        _write scope;
-        put "; ";
-        _write expr
+       fprintf ppf "with %a; %a" _write scope _write expr 
   in
-  pp_open_box formatter 0;
-  _write t;
-  pp_close_box formatter ();
-  pp_print_newline formatter ()
+  fprintf ppf "@[%a]@." _write t
+
+let write dest (t : t) =
+  let open Format in
+  let ppf = formatter_of_out_channel dest in
+  write ppf t
