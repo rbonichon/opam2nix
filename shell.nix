885f998f053d8efef7b62bd027f4d3dd9902971a
Richard Bonichon <richard.bonichon@tweag.io>
2021-01-28 09:49:19 +0100

    shell.nix: add hackish support for merlin & ocamlformat

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-let base = (import ./default.nix {}); in
+let
+  # this is probably a hackish way of getting a suitable development environment 
+  pkgs = import <nixpkgs> { };
+  ocamlDevel =
+    with pkgs.ocamlPackages; [ merlin pkgs.ocamlformat ];
+  base = (import ./default.nix { });
+in
 base.overrideAttrs (attrs: {
-	buildInputs = attrs.buildInputs ++ base.devInputs;
+  buildInputs =
+    attrs.buildInputs ++ base.devInputs ++ ocamlDevel;
 })
