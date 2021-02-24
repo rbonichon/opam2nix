7ec76528f656890943aa5d9ad256e62790223bfb
Richard Bonichon <richard.bonichon@tweag.io>
2021-02-24 11:26:49 +0100

    dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+(;; (nil . ((projectile-project-compilation-cmd . "nix-shell --run make")
+ ;;         (projectile-project-test-cmd . "pytest -s  tests_python/tests/test_mockup.py")))
+ ;; Format caml code on save
+ (tuareg-mode (mode . ocaml-format-on-save))
+ )
